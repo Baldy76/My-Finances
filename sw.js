db2b36f2a7e2e34351b59f2f1be9655387abb3db
Baldy76 <rgleech76@gmail.com>
2026-03-29 19:37:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finance-ios-v3.1.0';
+const CACHE_NAME = 'finance-ios-v3.1.1';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
