64f5fae98801b5cd7d1284bbb80a5cb9a2c8d8b3
Baldy76 <rgleech76@gmail.com>
2026-03-29 19:19:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,15 @@
-const CACHE_NAME = 'finance-ios-v2.7.0';
-const ASSETS = [
-    './',
-    './index.html',
-    './styles.css',
-    './app.js',
-    './manifest.json',
-    './my_finances.png'
-];
+const CACHE_NAME = 'finance-ios-v2.8.1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            );
-        })
-    );
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
